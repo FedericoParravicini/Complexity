15e33decb5b8f6cb46353271d36f22ef6aa1427e
gdenaro73 <denaro@disco.unimib.it>
2018-04-04 12:26:52 +0200

    symbolic execution with stub behavior

--- Complexity/src/complexity/se/ConstraintStub.java
+++ Complexity/src/complexity/se/ConstraintStub.java
@@ -12,9 +12,18 @@ public class ConstraintStub extends Constraint {
 		theConstraint = chars.charAt(nextChar++) + " > 0";
 	}
 
+	private ConstraintStub(String constraint) {
+		theConstraint = constraint;
+	}
+
 	@Override
 	public String toString() {
 		return theConstraint;
+	}
+
+	@Override
+	public Constraint mkNot() {
+		return new ConstraintStub("not (" + this.theConstraint + ")");
 	}	
 
 }
